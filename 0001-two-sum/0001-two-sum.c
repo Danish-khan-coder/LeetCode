5a83a112c6b6807c10ab6733098ff5725ef6a39d
Danish-khan-coder <khandanishasad65@gmail.com>
2026-08-09 23:12:49 +0530

    Time: 0 ms (100%), Space: 10.3 MB (5.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,26 +1,60 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#include <stdlib.h>
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* result =(int*)malloc(2*sizeof(int));
-    
-    for(int i=0;i<numsSize-1;i++){
 
-        
-            for(int j=i+1;j<numsSize;j++){
+    int* result = (int*)malloc(2 * sizeof(int));
 
-                if(nums[i]+nums[j]==target){
-                    result[0]=i;
-                    result[1]=j;
-                    *returnSize=2;
-                    return result;
-                    break;
-                }
+    // Hash table size
+    int tableSize = numsSize * 2 + 1;
+
+    int* keys = (int*)malloc(tableSize * sizeof(int));
+    int* values = (int*)malloc(tableSize * sizeof(int));
+    int* used = (int*)calloc(tableSize, sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+
+        int complement = target - nums[i];
+
+        // Find complement in hash table
+        unsigned int hash = (unsigned int)complement % tableSize;
+
+        while (used[hash]) {
+
+            if (keys[hash] == complement) {
+
+                result[0] = values[hash];
+                result[1] = i;
+
+                *returnSize = 2;
+
+                free(keys);
+                free(values);
+                free(used);
+
+                return result;
             }
-        
-       
+
+            hash = (hash + 1) % tableSize;
+        }
+
+        // Store current number
+        hash = (unsigned int)nums[i] % tableSize;
+
+        while (used[hash]) {
+            hash = (hash + 1) % tableSize;
+        }
+
+        used[hash] = 1;
+        keys[hash] = nums[i];
+        values[hash] = i;
     }
-       free(result);
-        *returnSize=0;
-        return NULL;
+
+    *returnSize = 0;
+
+    free(keys);
+    free(values);
+    free(used);
+    free(result);
+
+    return NULL;
 }
